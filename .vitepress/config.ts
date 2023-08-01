4b3efe7b1632689d2bc2f9f5f3d675e623769021
Hirsch Adway <adwayweb@gmail.com>
2023-08-01 20:29:49 +0330

    add base to config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:'/docs/',
   title: "My Codepress site",
   description: "a vitepress for mycodes",
   themeConfig: {
